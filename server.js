1e01e2fecaa7638975aa21d3444a29a14f91b6c9
unnati2000 <unnatibamania8@gmail.com>
2021-05-16 15:38:35 +0530

    mongodb setup

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const connectDB = require("./config/db");
+
 const app = express();
 const dotenv = require("dotenv");
 
 app.use(express.json());
+
 dotenv.config();
+
+connectDB();
 
 app.listen(5000, () => {
   console.log("Server running on PORT 5000");
